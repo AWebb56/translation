1e4722e61db684bf50e6a32e7b4fee319a025905
aetizazsameer <asameer@princeton.edu>
2024-04-24 23:21:17 -0400

    Update test.py parts of speech

--- test.py
+++ test.py
@@ -1,9 +1,20 @@
 import pandas
-df= pandas.read_csv("Noun.csv", header=None)
-words = {}
-for row in df.iterrows():
-    row = row[1].tolist()
-    if str(row[0])[0] in words:
-        words[str(row[0])[0]].append(row)
-    else:
-        words[str(row[0])[0]] = [row]
+
+def csv_to_dict(csv_filename):
+    dt = pandas.read_csv(csv_filename, header=None)
+    data = {}
+    for row in dt.iterrows():
+        row = row[1].tolist()
+        char = str(row[0])[0]
+        if char in data:
+            data[char].append(row)
+        else:
+            data[char] = [row]
+    return data
+
+def main():
+    filenames = ["Noun.csv", "Verb.csv", "Adj.csv", "Adverb.csv", "Symbol.csv"]
+    noun, verb, adj, adv, symbol = [csv_to_dict(filename) for filename in filenames]
+
+if __name__ == "__main__":
+    main()
